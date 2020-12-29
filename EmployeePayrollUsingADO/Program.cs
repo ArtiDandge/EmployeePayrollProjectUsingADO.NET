197eed8022d03d395c099f3ff37569e67f5beb48
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-29 15:49:26 +0530

    [Arti Dandge] Add : Display Welcome Message on console

---
+++ EmployeePayrollUsingADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollUsingADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service Project Using ADO.NET Framework!");
+        }
+    }
+}
